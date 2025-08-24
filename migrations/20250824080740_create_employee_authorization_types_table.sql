e09a9728090111f00a21a18e8ed632c87850023b
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-08-24 10:33:48 +0200

    add employee tables

---
+++ migrations/20250824080740_create_employee_authorization_types_table.sql
@@ -0,0 +1,14 @@
+-- Migration: Create employee authorization types table
+CREATE TABLE IF NOT EXISTS public."employee_authorization_types" (
+    pk_employee_authorization_type_id SERIAL PRIMARY KEY,
+    fk_employee_authorization_id INTEGER NOT NULL,
+    crud_type "CrudType" DEFAULT 'R',
+    description VARCHAR(500) NOT NULL,
+
+    CONSTRAINT fk_employee_authorization_id
+    FOREIGN KEY (fk_employee_authorization_id)
+    REFERENCES employee_authorizations(pk_employee_authorization_id),
+
+    CONSTRAINT unique_crud_type_per_authorization
+    UNIQUE (fk_employee_authorization_id, crud_type)
+);
