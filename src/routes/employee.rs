456b8d59e04b2c9c99bb6f756a03a288ae2600a6
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-08-29 19:40:07 +0200

    Move endpoint

--- src/routes/employee.rs
+++ src/routes/employee.rs
@@ -18,12 +18,12 @@ pub fn protected_employees_routes(
 
     Router::new()
         .route("/employees", get(get_all_employees).route_layer(from_fn(with_required_permissions(vec![20]))))
-        .route("/employees/{id}/accreditations", get(get_employee_all_accreditations).route_layer(from_fn(with_required_permissions(vec![34]))))
         .route("/employees/{id}", get(get_employee_by_id).route_layer(from_fn(with_required_permissions(vec![20]))))
         .route("/employees/levels", get(get_all_levels).route_layer(from_fn(with_required_permissions(vec![33]))))
         .route("/employees/levels/{id}", get(get_level_by_id).route_layer(from_fn(with_required_permissions(vec![33]))))
         .route("/employees/authorizations", get(get_all_authorizations).route_layer(from_fn(with_required_permissions(vec![32]))))
         .route("/employees/accreditations", get(get_all_accreditations).route_layer(from_fn(with_required_permissions(vec![34]))))
+        .route("/employees/{id}/accreditations", get(get_employee_all_accreditations).route_layer(from_fn(with_required_permissions(vec![34]))))
         .layer(from_fn_with_state(
             auth_state,
             auth_middleware,
